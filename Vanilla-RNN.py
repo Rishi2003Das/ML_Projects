a353e747ce541e775394e76d944a9976bcae5089
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2024-10-31 13:32:38 +0530

    Update and rename min-char-rnn.py to Vanilla-RNN.py

--- Vanilla-RNN.py
+++ Vanilla-RNN.py
@@ -3,9 +3,6 @@ Minimal character-level Vanilla RNN model. Written by Andrej Karpathy (@karpathy
 BSD License
 """
 
-## add comments by weixsong
-## reference page [The Unreasonable Effectiveness of Recurrent Neural Networks](http://karpathy.github.io/2015/05/21/rnn-effectiveness/)
-
 ## this is a 3 layers neuron network.
 ## input layer: one hot vector, dim: vocab * 1
 ## hidden layer: LSTM, hidden vector: hidden_size * 1
